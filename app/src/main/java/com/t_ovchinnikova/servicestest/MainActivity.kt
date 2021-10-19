8379335f72b8280c367528609c943ed3b5f4827e
TOvchinnikova <gibadullinatanya@gmail.com>
2021-10-19 15:48:09 +0500

    WorkManager

--- app/src/main/java/com/t_ovchinnikova/servicestest/MainActivity.kt
+++ app/src/main/java/com/t_ovchinnikova/servicestest/MainActivity.kt
@@ -13,6 +13,8 @@ import android.os.Bundle
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import androidx.core.content.ContextCompat
+import androidx.work.ExistingWorkPolicy
+import androidx.work.WorkManager
 import com.t_ovchinnikova.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -63,6 +65,14 @@ class MainActivity : AppCompatActivity() {
         }
         binding.jobIntentService.setOnClickListener {
             MyJobIntentService.enqueue(this, page++)
+        }
+        binding.workManager.setOnClickListener {
+            val workManager = WorkManager.getInstance(applicationContext)
+            workManager.enqueueUniqueWork(
+                MyWorker.WORK_NAME,
+                ExistingWorkPolicy.APPEND, //что делать, если какой-то воркер был уже запущен и мы запускаем новый
+                MyWorker.makeRequest(page++)
+            )
         }
     }
 }
